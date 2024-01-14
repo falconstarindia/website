12e24c0c6eb810230519d6d4155562e296129a3f
Prasanna <mail.prasanna.v@gmail.com>
2024-01-14 18:14:09 +0530

    adding duffle bag images

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,21 @@
+// Import fast-glob package
+const fg = require('fast-glob');
+
+// Run search for images in /gallery and /sponsors
+const duffleBags = fg.sync(['**/assets/images/featured-bags/duffle/*', '!**/_site']);
+
 module.exports = function (eleventyConfig) {
     // Copy static assets over to _site directory.
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/assets/images');
     eleventyConfig.addPassthroughCopy('src/js');
     eleventyConfig.addPassthroughCopy('src/assets/fonts');
+
+    eleventyConfig.addCollection('duffle', function(collection) {
+      console.log(duffleBags)
+      return duffleBags;
+    });
+  
     // Return configuration object.
     return {};
   };
